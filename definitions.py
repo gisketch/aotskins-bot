67b777683c39381fe823692f8183145a548b082d
ghegi <ghegi44@gmail.com>
2021-03-01 22:00:19 +0800

    Update definitions.py

--- definitions.py
+++ definitions.py
@@ -21,12 +21,12 @@ for c in cfg:
 	debug    = c['debug']
 	debug    = bool(strtobool(debug)) # Otherwhise, it's a string.
 
-db           = TinyDB(encryption_key=key, path=join_path("db/","profile.reto"), storage=tae.EncryptedJSONStorage) #Database file: stores points of every user.
-srv          = TinyDB(encryption_key=key, path=join_path("db/","srv.reto"), storage=tae.EncryptedJSONStorage) #Server-specific configuration - allows you to modify stuff like the name of the reactions, for example.
-activity     = TinyDB(encryption_key=key, path=join_path("db/","activity.reto"), storage=tae.EncryptedJSONStorage) #Activity file: the "Playing" commands the bot has.
-post         = TinyDB(encryption_key=key, path=join_path("db/","comments.reto"), storage=tae.EncryptedJSONStorage) #Comment leaderboard.
-priv         = TinyDB(encryption_key=key, path=join_path("db/","blacklist.reto"), storage=tae.EncryptedJSONStorage) #Privacy Mode blacklist. Users with PM on will not have their messages logged in the comment leaderboard.
-best         = TinyDB(encryption_key=key, path=join_path("db/","best.reto"), storage=tae.EncryptedJSONStorage) #Best Of name: Used to look up the Best-Of name of the channel.
-dm           = TinyDB(encryption_key=key, path=join_path("db/","deletables.reto"), storage=tae.EncryptedJSONStorage) #Message deletion for Leaderboards.
-customprefix = TinyDB(encryption_key=key, path=join_path("db/","customprefix.reto"), storage=tae.EncryptedJSONStorage) #Prefix file: custom prefixes per server.
-chan         = TinyDB(encryption_key=key, path=join_path("db/","channels.reto"), storage=tae.EncryptedJSONStorage) #Channel file: Used for Autovotes and other channel specific features.
+db           = TinyDB("db/profile.json") #Database file: stores points of every user.
+srv          = TinyDB("db/srv.json") #Server-specific configuration - allows you to modify stuff like the name of the reactions, for example.
+activity     = TinyDB("db/activity.json") #Activity file: the "Playing" commands the bot has.
+post         = TinyDB("db/post.json") #Comment leaderboard.
+priv         = TinyDB("db/priv.json") #Privacy Mode blacklist. Users with PM on will not have their messages logged in the comment leaderboard.
+best         = TinyDB("db/best.json") #Best Of name: Used to look up the Best-Of name of the channel.
+dm           = TinyDB("db/dm.json") #Message deletion for Leaderboards.
+customprefix = TinyDB("db/customprefix.json") #Prefix file: custom prefixes per server.
+chan         = TinyDB("db/chan.json") #Channel file: Used for Autovotes and other channel specific features.
