e665dff3f7a76bcfd333bf07b064dee9d15c220c
gisketch <agjmnz@gmail.com>
2021-03-04 17:14:33 +0800

    Update sharedFunctions.py

--- sharedFunctions.py
+++ sharedFunctions.py
@@ -151,14 +151,14 @@ async def getProfile(author, ctx, self):
 	embed=discord.Embed(title=author.name + ' ' + leaderemblem)
 	embed.set_thumbnail(url=author.avatar_url)
 	if result:
-		embed.add_field(name="Thanks Received", value=result.get('points'), inline=False)
+		embed.add_field(name="Thanks Received", value=str(result.get('points')), inline=False)
 	else:
 		embed.add_field(name="Thanks Received", value='0', inline=False)
 	if result:
 		embed.add_field(name="Artist Rank", value=roleemblem, inline=False)
-		embed.add_field(name="Leaderboard Rank", value=leadervalue, inline=False)
-		embed.add_field(name="Thanks Given", value=len(sentpoints), inline=False)
-		embed.add_field(name="Stars received", value=starsrec, inline=False)
+		embed.add_field(name="Leaderboard Rank", value=str(leadervalue), inline=False)
+		embed.add_field(name="Thanks Given", value=str(len(sentpoints)), inline=False)
+		embed.add_field(name="Stars received", value=str(starsrec), inline=False)
 	await ctx.send(embed=embed)
 
 #####
