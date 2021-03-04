9e40c08cfe3e75289bb9ab012c698adc7f558247
gisketch <agjmnz@gmail.com>
2021-03-04 17:21:49 +0800

    Update sharedFunctions.py

--- sharedFunctions.py
+++ sharedFunctions.py
@@ -91,9 +91,9 @@ async def getProfile(author, ctx, self):
 		elif leadervalue <= 10:
 			leaderemblem = "🏅"
 		else:
-			leaderemblem = " "
+			leaderemblem = "'\u200'"
 	else:
-		leaderemblem = " "
+		leaderemblem = "'\u200'"
 
 	#
 	# REVISAR ESTATUS DE CURATOR
